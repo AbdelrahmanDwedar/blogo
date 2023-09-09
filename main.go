f49ed7aca5c0e2b9d97a53e4ce93aae4eb5e65fd
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-09-09 21:42:39 +0300

    feat(env): add dotenv and server lsinter

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	godotenv.Load()
+
+	r := mux.NewRouter()
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf(":%s", os.Getenv("PORT")),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
